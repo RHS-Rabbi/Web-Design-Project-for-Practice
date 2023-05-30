ac439fa0d0a78f1fe05c4d23c5bfd4541d316879
Mohammad Rabbi Hossain <rabbihossain187@gmail.com>
2023-05-30 18:17:30 +0600

    Project-1

--- Project-1/main.js
+++ Project-1/main.js
@@ -9,8 +9,39 @@ $(document).ready(function () {
           autoplaySpeed: 500,
           animateOut: 'fadeOut',
           animateIn: 'fadeIn',
+          dots: false,
         }
       );
+
+      $('#web-progressbar').circleProgress({
+        value: .90,
+        size: 200,
+        thickness:2,
+        fill:'#0dbcc2',
+        emptyFill:'#fff'
+        }).on('circle-animation-progress', function(event, progress) {
+            $(this).find('.progressbar-parcentage').html(Math.round(90 * progress) + '<i>%</i>');
+        });;
+      
+        $('#graphic-progressbar').circleProgress({
+            value: .95,
+            size: 200,
+            thickness:2,
+            fill:'#0dbcc2',
+            emptyFill:'#fff'
+            }).on('circle-animation-progress', function(event, progress) {
+                $(this).find('.progressbar-parcentage').html(Math.round(95 * progress) + '<i>%</i>');
+            });;
+      
+        $('#digital-progressbar').circleProgress({
+                value: .84,
+                size: 200,
+                thickness:2,
+                fill:'#0dbcc2',
+                emptyFill:'#fff'
+                }).on('circle-animation-progress', function(event, progress) {
+                    $(this).find('.progressbar-parcentage').html(Math.round(84 * progress) + '<i>%</i>');
+                });;
 });
 
 
