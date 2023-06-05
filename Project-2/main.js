37958c6bc1bd8a5e9bfa0e5929fb20d275b909c9
Mohammad Rabbi Hossain <rabbihossain187@gmail.com>
2023-06-05 14:56:31 +0600

    Project-2

---
+++ Project-2/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+    $('#owl-1 .owl-carousel').owlCarousel(
+        {
+          items: 3,
+          margin: 50,
+          loop: true,
+          nav: false,
+          navText: ['Back','Next'],
+          dots: false,
+          dotsEach: true,
+          autoplay: false,
+          autoplaySpeed: 500,
+          animateOut: 'fadeOut',
+          animateIn: 'fadeIn',
+        }
+      );
+
+});
+
+
+
+
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
