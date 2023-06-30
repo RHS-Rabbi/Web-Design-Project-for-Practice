c5af3e284f3163e2fa40f202aca5739b5e663f1e
Mohammad Rabbi Hossain <rabbihossain187@gmail.com>
2023-06-30 21:04:09 +0600

    Project-9

---
+++ Project-9/src/main.js
@@ -0,0 +1,28 @@
+$('#owl-1 .owl-carousel').owlCarousel({
+    loop:true,
+    items: 1,
+    margin:10,
+    nav:false,
+    dots:true,
+})
+$('#owl-2 .owl-carousel').owlCarousel({
+    loop:true,
+    items: 1,
+    margin:10,
+    nav:true,
+    dots:true,
+})
+$('#owl-3 .owl-carousel').owlCarousel({
+    loop:true,
+    items: 1,
+    margin:10,
+    nav:true,
+    dots:true,
+})
+$('#owl-4 .owl-carousel').owlCarousel({
+    loop:true,
+    items: 1,
+    margin:10,
+    nav:true,
+    dots:true,
+})
